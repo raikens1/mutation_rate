6952b0ef5fb34d6a4f2b2c11a95a2c11362ec51e
Rachael Aikens <raikens1@swarthmore.edu>
2017-07-24 15:34:44 -0400

    added COSMO

---
+++ singletons_excluded/COSMO/cosmo_SNP_lists/awkcut_cosmo.sh
@@ -0,0 +1,46 @@
+bgzip -cd chrX_COSMO.gz | cut -f1-9 > chrX.temp
+bgzip -c chrX.temp > chrX_COSMO.gz
+bgzip -cd chr22_COSMO.gz | cut -f1-9 > chr22.temp
+bgzip -c chr22.temp > chr22_COSMO.gz
+bgzip -cd chr21_COSMO.gz | cut -f1-9 > chr21.temp
+bgzip -c chr21.temp > chr21_COSMO.gz
+bgzip -cd chr20_COSMO.gz | cut -f1-9 > chr20.temp
+bgzip -c chr20.temp > chr20_COSMO.gz
+bgzip -cd chr19_COSMO.gz | cut -f1-9 > chr19.temp
+bgzip -c chr19.temp > chr19_COSMO.gz
+bgzip -cd chr18_COSMO.gz | cut -f1-9 > chr18.temp
+bgzip -c chr18.temp > chr18_COSMO.gz
+bgzip -cd chr17_COSMO.gz | cut -f1-9 > chr17.temp
+bgzip -c chr17.temp > chr17_COSMO.gz
+bgzip -cd chr16_COSMO.gz | cut -f1-9 > chr16.temp
+bgzip -c chr16.temp > chr16_COSMO.gz
+bgzip -cd chr15_COSMO.gz | cut -f1-9 > chr15.temp
+bgzip -c chr15.temp > chr15_COSMO.gz
+bgzip -cd chr14_COSMO.gz | cut -f1-9 > chr14.temp
+bgzip -c chr14.temp > chr14_COSMO.gz
+bgzip -cd chr13_COSMO.gz | cut -f1-9 > chr13.temp
+bgzip -c chr13.temp > chr13_COSMO.gz
+bgzip -cd chr12_COSMO.gz | cut -f1-9 > chr12.temp
+bgzip -c chr12.temp > chr12_COSMO.gz
+bgzip -cd chr11_COSMO.gz | cut -f1-9 > chr11.temp
+bgzip -c chr11.temp > chr11_COSMO.gz
+bgzip -cd chr10_COSMO.gz | cut -f1-9 > chr10.temp
+bgzip -c chr10.temp > chr10_COSMO.gz
+bgzip -cd chr9_COSMO.gz | cut -f1-9 > chr9.temp
+bgzip -c chr9.temp > chr9_COSMO.gz
+bgzip -cd chr8_COSMO.gz | cut -f1-9 > chr8.temp
+bgzip -c chr8.temp > chr8_COSMO.gz
+bgzip -cd chr7_COSMO.gz | cut -f1-9 > chr7.temp
+bgzip -c chr7.temp > chr7_COSMO.gz
+bgzip -cd chr6_COSMO.gz | cut -f1-9 > chr6.temp
+bgzip -c chr6.temp > chr6_COSMO.gz
+bgzip -cd chr5_COSMO.gz | cut -f1-9 > chr5.temp
+bgzip -c chr5.temp > chr5_COSMO.gz
+bgzip -cd chr4_COSMO.gz | cut -f1-9 > chr4.temp
+bgzip -c chr4.temp > chr4_COSMO.gz
+bgzip -cd chr3_COSMO.gz | cut -f1-9 > chr3.temp
+bgzip -c chr3.temp > chr3_COSMO.gz
+bgzip -cd chr2_COSMO.gz | cut -f1-9 > chr2.temp
+bgzip -c chr2.temp > chr2_COSMO.gz
+bgzip -cd chr1_COSMO.gz | cut -f1-9 > chr1.temp
+bgzip -c chr1.temp > chr1_COSMO.gz
