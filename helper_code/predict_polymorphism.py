8a841812ad606af78a2d5da6a76f27cdb9ac1209
Rachael Aikens <raikens1@swarthmore.edu>
2017-07-31 17:29:27 -0400

    tested main function.

--- helper_code/predict_polymorphism.py
+++ helper_code/predict_polymorphism.py
@@ -20,7 +20,11 @@ def main():
         print "No arguements specified.  Exiting!"
         exit(0)
 
+    print "Reading in model parameters from " + argv[2]
     pred = Predictor(argv[2], bool(int(argv[3])))
+
+    print "Calculating parameters for regions in " + argv[1]
+    pred.getParams(argv[1])
 
 
 
@@ -66,36 +70,27 @@ class Predictor(object):
 
     # calculate parameters for sequence
     def getParams(self, bedfile):
-        with open(bedfile) as b:
-            for line in bedfile:
+        with open(bedfile, 'r') as b:
+            for line in b:
+                print line
                 row = line.split('\t')
-                if line.startswith(">"): # skip comment line
+                print row
+                if not line.startswith("chr"): # skip comment line
                     continue
-                elif row[0] != self.chrom:
-                    self.switchChrom(row[0])
+                elif row[0][3:] != self.chrom:
+                    print row[0]
+                    self.switchChrom(row[0][3:])
                 self.parseBedLine(row)
 
     # load new chrom file
     def switchChrom(self, chrom):
+        print "Loading new chromosome sequence."
         self.ref_genome_chr = SeqIO.read('/project/voight_datasets/hg19/chr'+chrom+'.fa', "fasta")
-
-    # read through a line of a bed file and add parameters for each position
-    def parseBedLine(self, row):
-        start = int(row[1])
-        stop = int(row[2])
-        for i in range(start+1, stop+1):
-            seq = self.get_context(i)
-            if "N" in seq:
-                continue
-            elif seq in self.prob:
-                self.params.append(self.prob[seq])
-            else:
-                self.params.append(self.prob[self.reverse_comp(seq)])
-        print self.prob
+        self.chrom = chrom
 
     # given position, get context
-	def get_context(self, pos):
-		return str(self.ref_genome_chr.seq)[pos-(self.flank+1):pos+self.flank].upper()
+    def get_context(self, pos):
+        return str(self.ref_genome_chr.seq)[pos-(self.flank+1):pos+self.flank].upper()
 
     # given a sequence, return reverse compliment
     def reverse_comp(self, sequence):
@@ -104,6 +99,25 @@ class Predictor(object):
             sequence_rc = self.compliments[char] + sequence_rc
 
         return sequence_rc
+
+    # read through a line of a bed file and add parameters for each position
+    def parseBedLine(self, row):
+        start = int(row[1])
+        stop = int(row[2])
+        for i in range(start+1, stop+1):
+            seq = self.get_context(i)
+            if "N" in seq:
+                print seq
+                continue
+            elif seq in self.prob:
+                p = self.prob[seq]
+                self.params.append(p)
+                self.mean += p
+            else:
+                p = self.prob[self.reverse_comp(seq)]
+                self.params.append(p)
+                self.mean += p
+        print self.mean
 
 if __name__ == '__main__':
     main()
