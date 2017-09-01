6be896829b83a4724f8acfd17bfc58336d4f0023
raikens1 <rockyaikens@gmail.com>
2017-09-01 15:31:34 -0400

    minor tinkering. reapplying model

--- analysis/workflows_finished/model_building/prediction/estimate_bychrom.sh
+++ analysis/workflows_finished/model_building/prediction/estimate_bychrom.sh
@@ -7,5 +7,5 @@ tcounts=( 519312 292615 332024 364931 424121 318476 375672 379454 323844 335170 
 
 for i in {1..22}; do
 	handle="predict_COSMO_AFR_chr${i}"
-	bsub -q voight_normal -o ${handle}.out -e ${handle}.err "predict_polymorphism.py ../../../../vcf_filtration/nc_bedfiles/nc_chr${i}_regions.bed ../results/AFR_3mer_model_params.txt 0 ${tcounts[`expr ${i} - 1`]}"
+	bsub -q voight_normal -o ${handle}.out -e ${handle}.err "predict_polymorphism.py ../../../../vcf_filtration/nc_bedfiles/nc_chr${i}_regions.bed ../results/AFR_3mer_model_params.txt 1 ${tcounts[`expr ${i} - 1`]}"
 done
