3338bc41b4f0a6faa7226347f9b181a984f9cd26
Rachael Aikens <raikens@BMIR-MACBOOK-SP1.hsd1.pa.comcast.net>
2018-03-28 17:11:48 -0400

    Supplementary figures 1 and 2; some R code updates

--- analysis/R_code/Chisq_plots.R
+++ analysis/R_code/Chisq_plots.R
@@ -86,9 +86,12 @@ sigs.plot <- function(p.vals, lab.lim, NoGGA = F){
   return(my.plot)
 }
 
-
 #volcano plot as in Harris 2015
-volcano.plot <- function(counts.1, counts.2, p.vals, lab.lim){
+volcano.plot <- function(counts.1, counts.2, 
+                         p.vals, lab.lim, 
+                         pops = c("Population 1", "Population 2"), 
+                         legpos = c(0.2, 0.75)){
+  
   f.1 <- counts.1$Count/sum(counts.1$Count)
   f.2 <- counts.2$Count/sum(counts.2$Count)
   f.diff <- (f.1 -f.2)/f.2
@@ -96,20 +99,22 @@ volcano.plot <- function(counts.1, counts.2, p.vals, lab.lim){
   
   sigs <- subset(counts.1, p.vals$p < lab.lim)
   f.diff.sigs <- subset(f.diff, p.vals$p < lab.lim)
-  p.sigs <- subset(ps, p.vals$p < lab.lim)
+  p.sigs <- subset(p.vals, p.vals$p < lab.lim)
   
   v_plot <- qplot(f.diff, -log10(p.vals$p)) +
-    labs(x = "Fold excess in East Asia", y = expression(-log[10](italic(p))), title = "East Asia v. Africa")+
-    scale_color_manual("", values = h.colors)+ #set colors
-    geom_text(data = sigs, aes(f.diff.sigs, -log10(p.sigs$p), label = sigs$Context, color = sigs$One_mer), 
-              vjust =0, nudge_y =.5, size = 3, check_overlap = TRUE)+ #label highly significant
-    geom_point(aes(color=factor(counts.1$One_mer)), size = 3)+ #Color pts by one_mer
-    geom_hline(yintercept = -log10(alpha), color = "black", linetype = 2)+#significance line
-    geom_text(aes(-.45, -log10(alpha)+1, label = "significant (p < 5e-4)", vjust = 0), color = "black", size = 3)+#significant text
-    theme(axis.text.x = element_text(size = rel(1.3)), axis.title.x = element_text(size = rel(1.3)), #adjust text sizes
-          axis.title.y = element_text(size = rel(1.3)), axis.text.y = element_text(size = rel(1.35)), 
-          legend.text = element_text(size = rel(1.2)), legend.position = c(0.8, 0.75),
-          title = element_text(size = rel(1.2)))#legend position and size
+    labs(x = paste("Fold excess in", pops[1]) , 
+         y = expression(-log[10](italic(p))), 
+         title = paste(pops[1], "v", pops[2])) +
+    scale_color_manual("", values = h.colors)+ # set colors
+    geom_text(data = sigs, aes(f.diff.sigs, -log10(p.sigs$p), label = sigs$Context, color = sigs$X1mer), 
+              vjust = 1, nudge_y =-1, hjust = 1, nudge_x = -.03, size = 3, check_overlap = TRUE)+ # label highly significant
+    geom_point(aes(color=factor(counts.1$X1mer)), size = 3)+ # Color pts by one_mer
+    geom_hline(yintercept = -log10(alpha), color = "black", linetype = 2)+# significance line
+    theme(axis.text.x = element_text(size = rel(1.3)), axis.title.x = element_text(size = rel(1.1)), # adjust text sizes
+          axis.title.y = element_text(size = rel(1.1)), axis.text.y = element_text(size = rel(1.35)), 
+          legend.text = element_text(size = rel(1.1)), legend.position = legpos,
+          title = element_text(size = rel(1.2))) + # legend position and size 
+    xlim(c(-.4, .6)) + ylim(c(0, 400))
   
   return(v_plot)
 }
