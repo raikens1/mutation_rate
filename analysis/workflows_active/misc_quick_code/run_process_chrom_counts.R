d757417288a2e9e83d6d7c782a111b0e4b986fdb
Rachael C. "Rocky" Aikens <rocky@DN52ec4m.SUNet>
2018-11-09 13:59:26 -0800

    first pass SGDP replication complete:

--- analysis/workflows_active/misc_quick_code/run_process_chrom_counts.R
+++ analysis/workflows_active/misc_quick_code/run_process_chrom_counts.R
@@ -7,10 +7,10 @@ ref <- read_delim("ref_files/3mer_mutations_ref.txt",
 gw_counts <- read_delim("gw_counts/gw_3mer_counts.txt", 
                         "\t", escape_double = FALSE, trim_ws = TRUE)
 
-cc <- read.delim("~/Documents/Research/mutation_rate/vcf_filtration/SGDP/raw_file_prep/3_private/SGDP_SAS_chrom_counts.txt")
+cc <- read.delim("~/Documents/Research/mutation_rate/vcf_filtration/SGDP/raw_file_prep/3_private/SGDP_AFR_chrom_counts.txt")
 
 processed_cc <- process_chrom_counts(chrom_counts = cc,
                      gw_counts = gw_counts, 
                      subcontext_ref = ref)
 
-write.table(processed_cc, "3mer/SGDP_SAS_counts.txt", quote = F, sep = "\t", row.names = F)
+write.table(processed_cc, "3mer/SGDP_AFR_counts.txt", quote = F, sep = "\t", row.names = F)
