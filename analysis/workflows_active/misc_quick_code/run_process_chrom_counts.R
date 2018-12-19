37dacc9becb535d06dda8f6509debae9eaf83e4f
Rachael C. "Rocky" Aikens <rocky@Rachaels-MacBook-Pro-2.local>
2018-12-19 16:18:55 -0500

    benchmarking Sections 1-3 of paper replication

--- analysis/workflows_active/misc_quick_code/run_process_chrom_counts.R
+++ analysis/workflows_active/misc_quick_code/run_process_chrom_counts.R
@@ -1,16 +1,16 @@
 setwd("~/Documents/Research/mutation_rate/analysis/data")
 source("../R_code/data_wrangling/process_chrom_counts.R")
 
-ref <- read_delim("ref_files/3mer_mutations_ref.txt", 
+ref <- read_delim("ref_files/7mer_mutations_ref.txt", 
                   "\t", escape_double = FALSE, trim_ws = TRUE)
 
-gw_counts <- read_delim("gw_counts/gw_3mer_counts.txt", 
+gw_counts <- read_delim("gw_counts/gw_7mer_counts.txt", 
                         "\t", escape_double = FALSE, trim_ws = TRUE)
 
-cc <- read.delim("~/Documents/Research/mutation_rate/vcf_filtration/SGDP/raw_file_prep/3_private/SGDP_AFR_subset_chrom_counts.txt")
+cc <- read.delim("~/Documents/Research/mutation_rate/vcf_filtration/SGDP/raw_file_prep/3_private/7mer_counts/SGDP_SAS_chrom_counts.txt")
 
 processed_cc <- process_chrom_counts(chrom_counts = cc,
                      gw_counts = gw_counts, 
                      subcontext_ref = ref)
 
-write.table(processed_cc, "3mer/SGDP_AFR_subset_3mer_counts.txt", quote = F, sep = "\t", row.names = F)
+write.table(processed_cc, "7mer/SGDP_SAS_7mer_counts.txt", quote = F, sep = "\t", row.names = F)
